496b748208b74c3ad4982d90811b2e3b971d2d68
Frank Natividad <contact@franknatividad.com>
2013-01-12 14:49:53 -0700

    Added Some comments to character creation

--- CharacterCreation.py
+++ CharacterCreation.py
@@ -23,17 +23,21 @@ class CharacterCreation ( ):
 		#This can be switched out with user control of some kind
 		#This works because the Jade Runner is waiting for CharacterCreationCompletedEvent ()
 		
+		#Send out Event that we are making a new Group called 0
+		#This will be the main group on screen.
 		ev = Events.NewGroupEvent ( 0 )
 		self.mediator.post ( ev )
-		
+		#Dummy Character
 		char = Character ( self.mediator , 0 )
 		char.setName ( "Name" + str(self.count) )
 		ev = Events.NewCharacterEvent ( char )
 		self.mediator.post ( ev )
 		self.count += 1
-
+		#Once Finished Creating Characters send event so we can proceed to the next step.
 		ev = Events.FinishedCharactersEvent ( )
 		self.mediator.post ( ev )
+		#Once Finished we should remove CharacterCreation from Mediator
+		self.mediator.removeObserver ( self )
 
 	def notify ( self , event ):
 		if isinstance ( event , Events.NewGameEvent ):
