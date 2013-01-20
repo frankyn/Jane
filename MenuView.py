c3726d1d512c7a5419cd13f40ddbba5a1e519cfc
Frank Natividad <contact@franknatividad.com>
2013-01-20 12:50:26 -0700

    Adding MenuView to the project.
    It is empty and just a shell for what is to come.

---
+++ MenuView.py
@@ -0,0 +1,10 @@
+#PyGame
+import pygame
+from pygame.locals import *
+
+#MenuView Class will contain all the necessary logic to render a menu screen inside the PyGame window
+class MenuView ( ):
+	def __init__ ( self ):	
+		pass
+
+	
